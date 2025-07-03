eee7b613e57dd0e02b4b751944a963d7ef657447
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-07-03 19:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (96.34%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string word = "a";
-        while(word.length() < k) {
-            // size is less than k
-            //inc the size of word, and appends the characters to the last
-            string temp = word;
-            for(int i=0; i<word.length(); i++) {
-                char c;
-                if(word[i] == 'z')
-                c = 'a';
-                else
-                c = word[i]+1;
-
-                temp += c;
-            }
-            word = temp;
-        }
-        return word[k-1];
+        int shift = __builtin_popcount(k-1);// counts number of set-bits
+        return 'a'+shift;
     }
 };
