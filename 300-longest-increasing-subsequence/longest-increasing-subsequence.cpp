8afc5e9e2bdbd7f9170e7b6b4a33ce1e463f891a
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-07-08 19:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (87.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int LIS(vector<int> &nums,int i,int prev,vector<vector<int>> &dp) {
-        if(i >= nums.size())
-        return 0;
-        if (prev != -1 && dp[i][prev] != -1)
-        return dp[i][prev];
-        int take = 0;
-        //take only, upcomming element is strictly greater than prev
-        if(prev == -1 || nums[prev] < nums[i])
-        take = 1 + LIS(nums,i+1,i,dp);
-        
-        int skip = LIS(nums,i+1,prev,dp);
-        if(prev != -1)
-        dp[i][prev] = max(take,skip);
-        return max(take,skip);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return LIS(nums,0,-1,dp);
+        // Patience Sort
+        vector<int> res;
+        for(int x : nums) {
+            auto it = lower_bound(res.begin(),res.end(),x);
+            if(it == res.end()) {
+                //not found
+                res.push_back(x);
+            }
+            else
+            *it = x; // replace with the smallest value
+        }
+        return res.size();
     }
 };
