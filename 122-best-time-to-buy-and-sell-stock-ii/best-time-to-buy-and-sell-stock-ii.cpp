5c7fa5f3040edad9cb58ca1a275c640d8e0b795f
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-07-26 00:51:03 +0530

    Time: 6 ms (25.36%) | Memory: 19.8 MB (30.03%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int fun(vector<int> &prices,int n,int i,int buy,vector<vector<int>> &dp) {
-        if(i == n)
-        return 0;
-        int profit = 0;
-        if(dp[i][buy] != -1)
-        return dp[i][buy];
-        if(buy) {
-            //buy
-            dp[i][1] = profit = max(-prices[i]+fun(prices,n,i+1,0,dp),0+fun(prices,n,i+1,1,dp)); // buy, not buy
-        }
-        else {
-            dp[i][0] = profit = max(prices[i]+fun(prices,n,i+1,1,dp),0+fun(prices,n,i+1,0,dp));
-        }
-        return profit;
-    }
     int maxProfit(vector<int>& prices) {
-        //recursion + memo
+        //bottom up -> traverse from the last, becoause the sequence of days matter
         int n = prices.size();
-        // 1-> buy
-        // 0-> sell
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return fun(prices,n,0,1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        dp[n][0] = dp[n][1] = 0;
+        for(int i=n-1; i>=0; i--) {
+            for(int buy = 0; buy<=1; buy++) {
+                int profit = 0;
+                if(buy) {
+                    //buy
+                    profit = max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
+                else
+                profit = max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
