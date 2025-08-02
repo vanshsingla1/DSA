52a318a29aad68c801fe02f70f04b10555a255e9
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-02 11:47:34 +0530

    Time: 11 ms (45.87%) | Memory: 17.9 MB (54.24%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    int fun(vector<int>& prices,int k,vector<vector<vector<int>>> &dp,int i,int buy,int nsell,int n) {
-        // similiar to knapsack
-        if(i == n)
-        return 0;
-        if(dp[i][buy][nsell] != -1)
-        return dp[i][buy][nsell];
-        if(buy) {
-            if(nsell < k) {
-                dp[i][buy][nsell] = max(-prices[i]+fun(prices,k,dp,i+1,0,nsell,n),0+fun(prices,k,dp,i+1,1,nsell,n));
-            }
-            else {
-                dp[i][buy][nsell] = fun(prices,k,dp,i+1,1,nsell,n);
-            }
-        }
-        else {
-                dp[i][buy][nsell] = max(prices[i]+fun(prices,k,dp,i+1,1,nsell+1,n),0+fun(prices,k,dp,i+1,0,nsell,n));
-        }
-        return dp[i][buy][nsell];
+    int fun(vector<int>& prices, vector<vector<int>>& dp, int i, int trans,
+            int n,int k) {
+        if (i == n || trans == k*2)
+            return 0;
+
+        if (dp[i][trans] != -1)
+            return dp[i][trans];
+        if (trans % 2 == 0) {
+            dp[i][trans] =
+                max(-prices[i] + fun(prices, dp, i + 1, trans + 1, n,k),
+                    0 + fun(prices, dp, i + 1, trans, n,k));
+        } else
+            dp[i][trans] = max(prices[i] + fun(prices, dp, i + 1, trans + 1, n,k),
+                               0 + fun(prices, dp, i + 1, trans, n,k));
+
+        return dp[i][trans];
     }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (k+1,-1)));
-        // 1-> buy
-        // 0-> sell
-        return fun(prices,k,dp,0,1,0,n); // i buy nsell
+        vector<vector<int>> dp(n, vector<int>(k*2, -1));
+        // trans = B S B S (0,1,2,3,...) even-> Buy, odd-> Sell
+        return fun(prices, dp, 0, 0, n,k); // i, trans
     }
 };
