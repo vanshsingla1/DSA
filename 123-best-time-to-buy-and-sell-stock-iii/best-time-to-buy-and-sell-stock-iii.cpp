403f818c5920d678d3fa25234e1446c80dd82520
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-02 10:43:57 +0530

    Time: 500 ms (8.98%) | Memory: 225.7 MB (8.93%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int fun(vector<int>& prices, int n, vector<vector<vector<int>>>& dp, int i, int buy, int nsell) {
-        if (i == n)
-            return 0;
+    int fun(vector<int>& prices,vector<vector<vector<int>>> &dp,int n,int i,int buy,int nsell) {
+        if(i == n)
+        return 0;
+        if(dp[i][buy][nsell] != -1)
+        return dp[i][buy][nsell];
 
-        if (dp[i][buy][nsell] != -1)
-            return dp[i][buy][nsell];
-
-        int profit = 0;
-
-        if (buy) {
-            if (nsell < 2) {
-                profit = max(
-                    -prices[i] + fun(prices, n, dp, i + 1, 0, nsell),
-                    fun(prices, n, dp, i + 1, 1, nsell)
-                );
-            } else {
-                profit = fun(prices, n, dp, i + 1, 1, nsell);
+        if(buy) {
+            // buy ,not buy
+            if(nsell < 2) {
+                dp[i][buy][nsell] = max(-prices[i]+fun(prices,dp,n,i+1,0,nsell),0+fun(prices,dp,n,i+1,1,nsell));
             }
-        } else {
-            profit = max(
-                prices[i] + fun(prices, n, dp, i + 1, 1, nsell + 1),
-                fun(prices, n, dp, i + 1, 0, nsell)
-            );
+            else {
+                dp[i][buy][nsell] = fun(prices,dp,n,i+1,1,nsell);
+            }
         }
-
-        return dp[i][buy][nsell] = profit;
+        else {
+            // sell, not sell
+            dp[i][buy][nsell] = max(prices[i]+fun(prices,dp,n,i+1,1,nsell+1),0+fun(prices,dp,n,i+1,0,nsell));
+        }
+        return dp[i][buy][nsell];
     }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return fun(prices, n, dp, 0, 1, 0);
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(3,-1)));
+        // 0-> sell
+        // 1-> buy
+        return fun(prices,dp,n,0,1,0); // i buy nsell
     }
 };
