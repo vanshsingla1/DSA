1f81cfa2905e2f1960330c26def3c443505fb122
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-05 18:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (38.76%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+    int fun(int n,vector<int> &dp) {
+        if(n <= 1)
+        return 1;
+        if(dp[n] != -1)
+        return dp[n];
+        int count = 0;
+        for(int i=1; i<=n; i++) {
+            count += fun(i-1,dp) * fun(n-i,dp);
+        }
+        return dp[n] = count;
+    }
     int numTrees(int n) {
         // Catalan numbers
         // pattern exactly matches with catalan numbers
         // n = 3 -> c0c2 + c1c1 + c2c0
-        if(n <= 1)
-        return 1;
-        int count = 0;
-        for(int i=1; i<=n; i++) {
-            count += numTrees(i-1) * numTrees(n-i);
-        }
-        return count;
+        vector<int> dp(n+1,-1);
+        return fun(n,dp);
     }
 };
