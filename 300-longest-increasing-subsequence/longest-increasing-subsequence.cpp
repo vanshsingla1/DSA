c461e18eb1f1c57daa0cccd504e9734acdcf148d
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-07 00:50:27 +0530

    Time: 583 ms (10.85%) | Memory: 296.8 MB (6.88%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        //Patience Sorting
-        int n = nums.size();
-        vector<int> sorted;
-        for(int i=0; i<n; i++) {
-            auto it = lower_bound(begin(sorted),end(sorted),nums[i]);
-            if(it == end(sorted)) {
-                //element is not found
-                sorted.push_back(nums[i]);
-            }
-            else {
-                *it = nums[i];// make new entry
-            }
+    int fun(vector<int>& nums, int n, int i, int prev,vector<vector<int>> &dp) {
+        if (i >= n)
+            return 0;
+        if (prev != -1 && dp[i][prev] != -1)
+        return dp[i][prev];
+        int take = 0;
+        int skip = 0;
+        if (prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + fun(nums, n, i + 1, i,dp);
         }
-        return sorted.size();
+        // skip
+        skip = fun(nums, n, i + 1, prev,dp);
+        if(prev != -1)
+        dp[i][prev] = max(take,skip);
+        return max(take, skip);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        // recursion
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return fun(nums, n, 0, -1,dp);
     }
 };
