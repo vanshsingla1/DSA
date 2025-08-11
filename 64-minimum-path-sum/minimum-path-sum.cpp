f0b99155222f5f232af9d4882e93272d62d99a33
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-11 11:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (14.57%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    int count(int m,int n,vector<vector<int>>&dp,vector<vector<int>>grid) {
-       //tabulation becoz in memoization tle is comming
-       for(int i=0; i<m; i++) {
-        for(int j=0; j<n; j++) {
-            if(i==0 && j==0) {
-                dp[i][j] = grid[i][j];
-                continue;
-            }
-            int up = 0;
-            int left = 0;
-            if(i > 0)
-            up = dp[i-1][j];
-            else
-            up = INT_MAX;
-            if(j > 0)
-            left = dp[i][j-1];
-            else
-            left = INT_MAX;
-            dp[i][j] = min(left,up) + grid[i][j];
-        }
-    }
-    return dp[m-1][n-1];
-}
+    
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return count(m,n,dp,grid);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        // intialisation of dp
+        dp[0][0] = grid[0][0];
+        // now fill the values for dp table
+        // first row
+        for(int i=1; i<n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        // first row
+        for(int i=1; i<m; i++) {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if(i-1 >= 0 && j-1>= 0) {
+                    dp[i][j] = grid[i][j] + min(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
