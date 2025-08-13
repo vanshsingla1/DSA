e875543f45b2367eedc8209944463b03d2f19578
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-13 10:36:26 +0530

    Time: 12 ms (5.68%) | Memory: 9.1 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    bool fun(int n) {
-        if(n == 1)
-        return true;
-        if (n < 1 || n % 3 != 0) 
-        return false;
-        return fun(n/3);
-    }
     bool isPowerOfThree(int n) {
-        if(n == 1)
-        return true;
-        return fun(n);
+        // 3^x == n
+        // x*log(3) = log(n)
+        // x = log(n)/log(3)
+        // if x is an int then return true otherwise false
+        if(n <= 0)
+        return false;
+        double x = log10(n)/log10(3);
+        return x == (int)x;
     }
 };
