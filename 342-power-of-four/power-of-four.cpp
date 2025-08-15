a909d4bf9e8aa2dce6c04c1552e69f3bf3144003
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-15 10:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (4.79%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    bool fun(int n) {
-        if(n == 1)
-        return true;
-        if(n < 1 || n%4 != 0)
-        return false;
-        return fun(n/4);
-    }
     bool isPowerOfFour(int n) {
         if(n <= 0)
         return false;
-        return fun(n);
+        double x = log10(n)/log10(4);
+        return x == (int)x;
     }
 };
