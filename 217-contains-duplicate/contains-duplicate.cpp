85151720106ddf7a2918a9ee01cdf054ab614655
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-19 00:17:48 +0530

    Time: 48 ms (66.25%) | Memory: 90.8 MB (62.22%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            mp[nums[i]]++;
+            if(mp[nums[i]] >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
