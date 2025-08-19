a0c7c6cf21fdcfe660221d205dd6cc0a3293bc53
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-19 10:57:07 +0530

    Time: 0 ms (100.00%) | Memory: 111.2 MB (95.09%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long count = 0, zeros = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                zeros++;
-                count += zeros;
+        // sliding window - O(n)
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+        long long count = 0;
+        
+        while(left < n) {
+            if(nums[left] == 0) {
+                right = left;
+                while(right < n && nums[right] == 0) {
+                    right++;
+                }
+                long long windowSize = right - left;
+                count += (windowSize * (windowSize + 1)) / 2;
+                left = right; 
             } else {
-                zeros = 0;
+                left++;
             }
         }
         return count;
