622428889b019cdc98715071fd8a349b324bcffa
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-08-25 19:28:21 +0530

    Time: 64 ms (8.91%) | Memory: 25.7 MB (10.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) 
-    {
-        int count=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<m; j++)
-            {
-                if(grid[i][j]=='1' && !visited[i][j])
-                {
+    void dfs(vector<vector<char>>& grid,vector<vector<int>> &visited,int i,int j) {
+        visited[i][j] = 1;
+        vector<int> dx = {-1,1,0,0};
+        vector<int> dy = {0,0,-1,1};
+        for(int k=0; k<4; k++) {
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+            if(nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && !visited[nx][ny] && grid[nx][ny] == '1') {
+                dfs(grid,visited,nx,ny);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        int count = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(!visited[i][j] && grid[i][j] == '1') {
+                    dfs(grid,visited,i,j);
                     count++;
-                    checkIslands(grid,i,j,n,m,visited);
                 }
             }
         }
         return count;
-    }
-    bool valid(vector<vector<char>>& grid, int i, int j, int n, int m,vector<vector<int>>& visited)
-    {
-        if(i>=0 && i<n && j>=0 && j<m && grid[i][j]=='1' && !visited[i][j])
-            {
-                return true;
-            }
-        return false;
-    }
-
-    void checkIslands(vector<vector<char>>& grid,int i,int j,int n,int m,vector<vector<int>>& visited)
-    {
-        
-        visited[i][j]=1;
-      //  grid[i][j]='0';
-        if(valid(grid,i+1,j,n,m,visited)) //Down
-        {
-            checkIslands(grid,i+1,j,n,m,visited);
-        }
-        if(valid(grid,i-1,j,n,m,visited)) //Up
-        {
-            checkIslands(grid,i-1,j,n,m,visited);
-        }
-        if(valid(grid,i,j-1,n,m,visited)) //Left
-        {
-            checkIslands(grid,i,j-1,n,m,visited);
-        }
-        if(valid(grid,i,j+1,n,m,visited)) //Right
-        {
-            checkIslands(grid,i,j+1,n,m,visited);
-        }
     }
 };
