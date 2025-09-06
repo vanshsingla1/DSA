b6e70d64d778271fa881b7334c43b3dde6608b66
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-09-06 10:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (71.77%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static bool comp(vector<int> &p1, vector<int> &p2) {
+        if(p1[0] == p2[0]) {
+            return p1[1] > p2[1];
+        }
+        return p1[0] < p2[0];
+    }
+    int numberOfPairs(vector<vector<int>>& points) {
+        // optimized
+        int n = points.size();
+        int count = 0;
+        sort(begin(points),end(points),comp);
+        for(int i=0; i<n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int maxy = INT_MIN;
+            for(int j=i+1; j<n; j++) {
+                
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                // check the condition that x2 >= x1 && y2 <= y1
+                if(y2 > y1) {
+                    continue;
+                }
+                if(y2 > maxy) {
+                    count++;
+                    maxy = y2;
+                }
+            }
+        }
+        return count;
+    }
+};
