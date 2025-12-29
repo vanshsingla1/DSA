ecbdf6e0ed62e7f0f58fe5281f2dbd8e59a662db
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2025-12-29 17:20:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (49.04%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    void fun(vector<int>& nums, vector<vector<int>>& res, vector<int>& temp,
-             unordered_set<int>& st) {
-        if (temp.size() == nums.size()) {
+//Space Optimized - swapping
+    void dopermute(int n,vector<int>& nums,vector<int>& temp,vector<vector<int>>&res)
+    {
+        if(n == nums.size())
+        {
             res.push_back(temp);
             return;
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (st.find(i) == st.end()) {
-                st.insert(i);
-                temp.push_back(nums[i]);
-                fun(nums, res, temp, st);
-                temp.pop_back();
-                st.erase(i);
-            }
+        for(int i=n; i<nums.size(); i++)
+        {
+            swap(nums[i],nums[n]);
+            temp.push_back(nums[n]);
+            dopermute(n+1,nums,temp,res);
+            temp.pop_back();
+            swap(nums[i],nums[n]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        int n = nums.size();
+        vector<vector<int>>res;
         if(nums.empty())
         return res;
-        vector<int> temp;
-        unordered_set<int> st;
-        fun(nums, res, temp, st);
+        vector<int>temp;
+        dopermute(0,nums,temp,res);
         return res;
     }
 };
