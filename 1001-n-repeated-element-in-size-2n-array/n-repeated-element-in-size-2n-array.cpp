1e48eec3c012a9e05185747235f7ae780804a0a3
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2026-01-02 11:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (87.61%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        int n = nums.size()/2;
-        unordered_map<int,int> mp;
-        for(int i=0; i<nums.size(); i++) {
-            mp[nums[i]]++;
-            if(mp[nums[i]] == n) {
-                return nums[i];
+        int n = nums.size();
+        if(nums[0] == nums[1]) {
+            return nums[0];
+        }
+        if(nums[0] == nums[2] || nums[1] == nums[2]) {
+            return nums[2];
+        }
+        // moore voting 
+        int count = 1;
+        int element = nums[3];
+        for(int i=4; i<nums.size(); i++) {
+            if(count == 0) {
+                count++;
+                element = nums[i];
+            }
+            else if(nums[i] != element) {
+                count--;
+            }
+            else {
+                count++;
             }
         }
-        return -1;
+        return element;
     }
 };
