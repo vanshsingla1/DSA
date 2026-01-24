6a94a869f4775da875d870408acff49047699403
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2026-01-24 13:18:46 +0530

    Time: 53 ms (98.29%) | Memory: 198.7 MB (45.24%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,34 +1,55 @@
 class Solution {
 public:
-    void fun(double l, double h, vector<vector<int>>& squares, double target, double& result){
-        while(h-l > 1e-5){
-            double mid = l + (h-l)/2.0;
-            double sum = 0.0;
-            for(const auto&  vec : squares){
-                int x1 = vec[0], y1 = vec[1], side = vec[2];
-                if(y1 >= mid) continue;
-                double y2 = min((double)(y1+side), mid);
-                sum += side * (y2-y1);
-            }
+    //  double funhigher(vector<vector<int>>& squares, long double k) {
+    //     double area = 0.0;
+    //     for (auto square : squares) {
+    //          double height =
+    //             max(( double)0.0,
+    //                 min(( double)square[1] + square[2] - k,
+    //                     ( double)square[2]));
+    //         area += ( double)square[2] * height;
+    //     }
+    //     return area;
+    // }
 
-            if(sum >= target){
-                result = mid;
-                h = mid;
-            } else{
-                l = mid;
-            }
+     double funlower(vector<vector<int>>& squares,  double k) {
+         double area = 0.0; // width * height
+        for (auto& square : squares) {
+             double y_start = square[1];
+             double side = square[2];
+             double height = max((double)0.0, min(k - y_start, side));
+            area += side * height;
         }
+        return area;
     }
+
     double separateSquares(vector<vector<int>>& squares) {
-        double totalSum = 0.0;
-        double h=0.0;
-        for(const auto& vec : squares){
-            totalSum += ((double)vec[2]*vec[2]);
-            h =  max(h, (double)vec[1]+vec[2]);
+        // find max(y1,y2,y3,....)
+         double maxy = 0.0;
+         double miny = 1e18;
+         double totalarea = 0.0;
+        for (auto& square : squares) {
+            maxy = max(maxy, ( double)square[1] + square[2]);
+            miny = min(miny, ( double)square[1]);
+            totalarea += ( double)square[2] * square[2];
         }
-        double target = totalSum/2.0;
-        double result=h; 
-        fun(0.0, h, squares, target, result);
-        return result;
+
+         double low = miny;
+         double high = maxy;
+         double k = 0.0;
+         double areal = 0.0;
+
+        while (high-low > 1e-5) {
+            k = (low + high) / 2.0;
+            // calculate areal and areah
+            areal = funlower(squares, k); // calculates the area lower k
+            // areah = funhigher(squares, k);    no need
+
+            if (areal * 2.0 >= totalarea)
+                high = k;
+            else
+                low = k;
+        }
+        return (double)k;
     }
 };
