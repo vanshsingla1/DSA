d1f019446b2f6060f291ee2388992834d3361f0d
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2026-01-31 11:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (24.67%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        // letters -> non-decreasing order
-        // atleast two different characters in letters
-        // only single character target , find the smallest character in letters that is lexicographically greater than target
-        int n = letters.size();
-        priority_queue<char,vector<char>,greater<char>> pq;
-        for(int i=0; i<n; i++) {
-            if(letters[i] > target) pq.push(letters[i]);
+        for(auto ch : letters) {
+            if(ch > target) return ch;
         }
-        if(!pq.empty()) return pq.top();
-        else return letters[0];
+        return letters[0];
     }
 };
