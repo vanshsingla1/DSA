eb2c98192265dc25109b298fc6ebb07b74a3be1a
Vansh Singla <176417201+vanshsingla1@users.noreply.github.com>
2026-02-02 18:13:26 +0530

    Time: 32 ms (28.03%) | Memory: 23.4 MB (90.71%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        // shortest path -> dijkstra 
         int m = grid.size();
         int n = grid[0].size();
-        if (grid[0][0] != 0 || grid[m-1][n-1] != 0) 
-        return -1;
-        // 8 directionoally
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
+        vector<vector<int>> dist(m,vector<int> (n,INT_MAX));
+        dist[0][0] = 0;
         vector<int> dx = {-1,1,0,0,1,1,-1,-1};
-        vector<int> dy = {0,0,-1,1,1,-1,-1,1}; // all 8 directions are covered
-        // tuple<int,int,int>
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>> pq;
-        pq.push({1,0,0});
-        grid[0][0] = 1;
+        vector<int> dy = {0,0,1,-1,1,-1,1,-1};
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        // pair<int,pair<int,int>> p
+        pq.push({0,{0,0}}); // dist x y
         while(!pq.empty()) {
-            auto [dist,x,y] = pq.top();
+            int d = pq.top().first;
+            pair<int,int> node = pq.top().second;
+            int x = node.first;
+            int y = node.second;
             pq.pop();
-            if(x == m-1 && y == n-1) {
-                return dist;
-            }
+            // if(d > dist[x][y]) continue;
             for(int i=0; i<8; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 0) {
-                    grid[nx][ny] = dist+1;
-                    pq.push({dist+1,nx,ny});
+                    if(dist[nx][ny] > d+1) {
+                        pq.push({d+1,{nx,ny}});
+                    dist[nx][ny] = d+1;
+                    }
                 }
             }
         }
-        return -1;
+        return dist[m-1][n-1] == INT_MAX ? -1 : dist[m-1][n-1] + 1;
     }
 };
